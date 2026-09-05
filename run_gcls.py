38edfd5a57a5d9a66bb1c5f3fb4315abac45c95a
thaksheel <tna324@lehigh.edu>
2026-09-05 10:23:42 -0400

    work in progress

--- run_gcls.py
+++ run_gcls.py
@@ -16,7 +16,7 @@ params = Params(
     M=3,
     Mlst=[3, 3],
     hid_dim=64,
-    epochs=200,
+    epochs=1000,
     lr=5e-3,
     wd=5e-3,
     dropout=0.65,
@@ -26,22 +26,20 @@ params = Params(
     device="cpu",
     batch_size=32,
 )
+names = ["MUTAG", "NCI109", "NCI1", "PROTEINS"]
+for name in names: 
+    dataset = TUDataset(root="data/TUDataset", name=name)  # 4100
+    params.num_features = dataset.num_features
+    params.num_classes = dataset.num_classes
 
-dataset = TUDataset(root="data/TUDataset", name="NCI109")  # 4100
-dataset = TUDataset(root="data/TUDataset", name="MUTAG")  # 186
-dataset = TUDataset(root="data/TUDataset", name="PROTEINS")  # 1100
-dataset = TUDataset(root="data/TUDataset", name="NCI1")  # 4100
-params.num_features = dataset.num_features
-params.num_classes = dataset.num_classes
-
-trainer = THGTrainer(params, display=True)
-eval_results = trainer.evaluate_graph_cls(dataset)
-best_results = trainer.get_best_eval_results(eval_results)
-df_results = trainer.evaluation_results_to_df(
-    eval_results,
-    outname="./exports/tu_results5.xlsx",
-    export=True,
-    exclude_fields=[],
-)
-print(f"\n---> best_restuls: {best_results}")
+    trainer = THGTrainer(params, display=True)
+    eval_results = trainer.evaluate_graph_cls(dataset)
+    best_results = trainer.get_best_eval_results(eval_results)
+    df_results = trainer.evaluation_results_to_df(
+        eval_results,
+        outname=f"./exports/tu_results{name.lower()}.xlsx",
+        export=True,
+        exclude_fields=[],
+    )
+    print(f"\n---> best_restuls: {best_results}")
 print("END")
